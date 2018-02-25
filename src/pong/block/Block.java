2653ecaa462b32914351a4c5fea9abac4b618df5
Connor Burns <ctrekker4@gmail.com>
2018-02-25 11:39:02 -0700

    Block Commenting

--- src/pong/block/Block.java
+++ src/pong/block/Block.java
@@ -3,13 +3,16 @@ package pong.block;
 import java.awt.*;
 
 public class Block {
+    // Default values for a block
     private final static int DEFAULT_STRENGTH=1;
     private final static int DEFAULT_X=0;
     private final static int DEFAULT_Y=0;
     private final static int DEFAULT_WIDTH=200;
     private final static int DEFAULT_HEIGHT=100;
-    private final static int BOX_RADIUS=5;
 
+    // List of colors which the block can be
+    // NOTE: This depends completely on the block's strength!
+    // @see private int strength;
     private static Color[] colors={
             new Color(0, 198, 0),
             new Color(220, 217, 0),
@@ -18,12 +21,19 @@ public class Block {
             new Color(10, 0, 144)
     };
 
+    // X pos, in pixels, of the box
     private int x;
+    // Y pos, in pixels, of the box
     private int y;
+    // Width, in pixels, of the box
     private int width;
+    // Height, in pixels, of the box
     private int height;
+    // The strength of the box.
+    // Strength is the number of hits a box takes to destroy
     private int strength;
 
+    // Make a variety of constructors for easier implementation
     public Block() {
         this(DEFAULT_X, DEFAULT_Y);
     }
@@ -41,19 +51,30 @@ public class Block {
         this.strength=strength;
     }
 
+    // Provide draw method for code organization
     public void draw(Graphics2D g2) {
+        // Set the color to the color based on strength
         g2.setColor(getColor());
+        // Fill a rectangle at box x and y
         g2.fillRect(x, y, width, height);
+        // Set color to white
         g2.setColor(Color.WHITE);
+        // If the box color is yellow, make the color black
+        // This is because white text on a yellow background just doesn't work :)
         if(getColor()==colors[1]) g2.setColor(Color.BLACK);
+        // Set the font to a smaller, unobtrusive font
         g2.setFont(new Font("Arial", Font.PLAIN, 10));
+        // Draw the strength value within the box itself
         g2.drawString(strength+"", x+3, y+10);
     }
 
+    // Returns a color based on strength
     public Color getColor() {
+        // Return a color based on strength, but if the strength is greater than
+        // the color array's length, loop back and recycle colors
         return colors[(strength-1)%colors.length];
     }
-
+    // More getters and setters
     public int getX() {
         return x;
     }
@@ -85,6 +106,7 @@ public class Block {
         this.strength = strength;
     }
 
+    // Provide debug info when printed
     public String toString() {
         return "Block[x="+x+",y="+y+",width="+width+",height="+height+",strength="+strength+"];";
     }
