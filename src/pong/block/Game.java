11838645baacc6eee8dbdb95a2abb989b23bbaf6
Connor Burns <ctrekker4@gmail.com>
2018-02-25 11:59:12 -0700

    pong.block.Game Commenting

--- src/pong/block/Game.java
+++ src/pong/block/Game.java
@@ -12,15 +12,25 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Game extends JFrame {
+    // player
     private Paddle player;
+    // List of balls. Currently unused, but multiple balls at once is completely supported!
     private ArrayList<Ball> balls;
+    // Speed of player's paddle
     private int paddleSpeed=6;
+    // Default speed of the ball
     private int defaultBallSpeed=4;
+    // Speed of the ball. Init to default
     private int ballSpeed=defaultBallSpeed;
+    // If this is true, the ball will go twice as fast
+    // This is controlled with the up arrow by the player
+    // That way, they can increase the rate of the game during slow moments
     private boolean speedUp=false;
+    // Current level the player is on. This determines the strength of boxes, and possibly arrangement
     private int currentLevel=1;
 
     public Game() {
+        // Init balls arrayList
         balls=new ArrayList<>();
 
         // Set frame properties
@@ -52,8 +62,11 @@ public class Game extends JFrame {
                         break;
                     // Down arrow
                     case 40:
+                        // Increase the level
                         currentLevel++;
+                        // Clear the current blocks list
                         graphics.blocks.clear();
+                        // Re-setup the blocks
                         graphics.setupBlocks(currentLevel);
                         break;
                 }
@@ -85,8 +98,11 @@ public class Game extends JFrame {
         setVisible(true);
     }
     private class GameGraphics extends Component {
+        // Make an arrayList for blocks
         public ArrayList<Block> blocks=new ArrayList<>();
 
+        // Lots of variables which control the state of the game
+        // They do exactly what they are named
         private boolean firstTime=true;
         private boolean gameOver=false;
         private int gameOverCount=0;
@@ -97,6 +113,7 @@ public class Game extends JFrame {
         private int lastBlockHit;
         private int lastBlockHitCount=0;
 
+        // Stores the current score
         private int score=0;
 
         public GameGraphics() {
@@ -109,29 +126,42 @@ public class Game extends JFrame {
             }, 0, (int)(1000/60.0));
         }
         public void paint(Graphics g) {
+            // Cast to higher-level graphics object
             Graphics2D g2=(Graphics2D)g;
 
+            // For some reason by default on some computers this is off.
+            // When it is off, circles and other arcs draw in extreme low resolution (they look like hexagons!)
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                     RenderingHints.VALUE_ANTIALIAS_ON);
 
+            // Do only on the first time
             if(firstTime) {
+                // Clear the block array
                 blocks.clear();
                 // set up blocks
                 setupBlocks(currentLevel);
 
+                // Init player x and y
                 player=new Paddle(0, 0);
+                // Switch orientation from up and down, to left and right
                 player.switchOrientation();
                 player.setX(getWidth()/2-player.getWidth()/2);
                 player.setY(getHeight()-player.getHeight()*2);
 
+                // Next time a new ball should be added. CURRENTLY UNIMPLEMENTED
                 ballThreshold=(int) Math.ceil(Math.random()*ballMultiplier);
 
+                // Add a ball at a randomish height
                 balls.add(new Ball(getWidth()/2, getHeight()-100));
+                // Make the direction x random
                 int dirX=(int)(Math.random()*2);
                 if(dirX==0) dirX=-1;
+                // Set the direction to this
                 balls.get(0).setDirection(new Point(dirX, 1));
+                // Make the deltaX very small so the ball essentially goes straight up/down the first time
                 balls.get(0).setDeltaX(0.00001);
 
+                // Make sure this isn't run again until the game is reset
                 firstTime=false;
             }
 
@@ -164,11 +194,19 @@ public class Game extends JFrame {
 
                     // Check for block collision
                     for(int i=0; i<blocks.size(); i++) {
+                        // This portion doesn't make sense at all in code.
+                        // The best way to understand how it works is on paper. If you'd like to see my math
+                        // just ask me for my paper which I figured out how to do the box collision. It is a
+                        // heck of a lot harder than it seems!
                         Block block=blocks.get(i);
+                        // Get the block center
                         int blockCenterX=block.getX()+block.getWidth()/2;
                         int blockCenterY=block.getY()+block.getHeight()/2;
+                        // Get the delta offset of the ball from the center on both axis
                         double deltaX=(Math.abs(blockCenterX-ball.getX()))/block.getWidth();
                         double deltaY=(Math.abs(blockCenterY-ball.getY()))/block.getHeight();
+                        // Init a boolean based on deltas. This determines whether collision should
+                        // be handled on the vertical (y) or horizonal (x) axis
                         boolean vertical=deltaX<deltaY;
 
                         // Check on x coordinate plane (bounce)
@@ -177,6 +215,7 @@ public class Game extends JFrame {
                                 &&ball.getX()+ball.getSize()/2>block.getX()
                                 &&ball.getX()-ball.getSize()/2<block.getX()+block.getWidth()
                                 &&!vertical) {
+                            // Bounce ball based on calculations
                             if(ball.getX()<block.getX()+block.getWidth()/2) {
                                 ball.setDirection(new Point(-Math.abs((int) ball.getDirection().getX()), (int) ball.getDirection().getY()));
                                 blockHit(i);
@@ -196,6 +235,7 @@ public class Game extends JFrame {
                                 &&ball.getY()+ball.getSize()/2>block.getY()
                                 &&ball.getY()-ball.getSize()/2<block.getY()+block.getHeight()
                                 &&vertical) {
+                            // Bounce ball based on calculations
                             if(ball.getY()<block.getY()+block.getHeight()/2) {
                                 ball.setDirection(new Point((int) ball.getDirection().getX(), -Math.abs((int) ball.getDirection().getY())));
                                 blockHit(i);
@@ -215,23 +255,30 @@ public class Game extends JFrame {
                         gameOver = true;
                     }
 
+                    // If the player wants to speed the ball up, increase its speed TEMPORARILY by 2
                     if(speedUp) ballSpeed*=2;
+                    // Set the x and y coordinates of the ball based on deltas, direction, and previous position
                     ball.setX((ball.getX() + ball.getDirection().getX() * ballSpeed * ball.getDeltaX()));
                     ball.setY((ball.getY() + ball.getDirection().getY() * ballSpeed * ball.getDeltaY()));
+                    // Reset the speed if it was sped up. That way it doesn't interfere with calculations
                     if(speedUp) ballSpeed/=2;
                 }
 
-
+                // Currently not implemented.
+                // Used to progressively add more balls on an exponential-time curve
                 if(ballCount>=ballThreshold) {
                     ballThreshold=(int) Math.ceil(Math.random()*ballMultiplier);
                     ballCount=0;
 
                     balls.add(randomBall());
                 }
+                // Increment ball frame counter
                 if(balls.size()<ballMax) ballCount++;
             }
             else {
+                // If its been 60 frames (a second), then...
                 if(gameOverCount>60) {
+                    // Reset all these instance variables so the next game will start similarly
                     gameOver=false;
                     gameOverCount=0;
                     firstTime=true;
@@ -242,11 +289,16 @@ public class Game extends JFrame {
                     score=0;
                     currentLevel=1;
                 }
+                // Increase the gameOver count
                 gameOverCount++;
             }
 
+            // Increment the lastBlockHit counter
             lastBlockHitCount++;
 
+            // If the last time a block has been hit was 2 seconds ago, there are no blocks left, and the player's
+            // ball is near the bottom of the screen, then increase the level and set up the next level's blocks
+            // on the screen
             if(lastBlockHitCount>120&&blocks.size()==0&&balls.get(0).getY()>getHeight()-getHeight()/10) {
                 currentLevel++;
                 lastBlockHitCount=0;
@@ -257,56 +309,76 @@ public class Game extends JFrame {
             g2.setColor(Color.BLACK);
             g2.fillRect(0, 0, getWidth(), getHeight());
 
+            // Draw the player
             player.draw(g2);
+            // Draw every ball
             for(Ball ball : balls) {
                 ball.draw(g2, true);
             }
-
+            // Draw every block
             for(Block block : blocks) {
                 block.draw(g2);
             }
 
+            // Draw the player's current score
             g2.setColor(Color.WHITE);
             g2.setFont(new Font("Arial", Font.PLAIN, 24));
             g2.drawString(score+"", 20, 40);
         }
+        // Handle when a block is hit
         private void blockHit(int i) {
             Block block=blocks.get(i);
+            // Make sure the lastBlock that was hit wasn't the same block, unless the lastBlockHit was 4 frames ago
             if(lastBlockHit!=i||lastBlockHitCount>=4) {
+                // If the strength is one, then...
                 if (block.getStrength() <= 1) {
+                    // Remove the block
                     blocks.remove(i);
+                    // Increment the score
                     score++;
-                } else {
+                }
+                // If not, then...
+                else {
+                    // Decrement the current block strength
                     block.setStrength(block.getStrength() - 1);
                 }
+                // Make the last block that was hit equal to 'i'
                 lastBlockHit=i;
             }
+            // Last hit was 0 frames ago as of this call
             lastBlockHitCount=0;
         }
 
+        // Set up level
+        // Has potential for unique levels based on shape, rather than procedural-style generation
         public void setupBlocks(int level) {
+            // Make a new list of balls
             ArrayList<Block> b=new ArrayList<>();
 
-            //if(level<=1) {
-                final int boxGap=10;
-                final int boxWidth=30;
-                final int boxHeight=15;
-                final int boxNumX=getWidth()/(boxWidth+boxGap);
-                final int boxNumY=5;
-                final int boxBaseX=((getWidth())-(boxWidth+boxGap)*boxNumX+boxGap)/2;
-                final int boxBaseY=50;
+            // Set block constants for generation
+            final int boxGap=10;
+            final int boxWidth=30;
+            final int boxHeight=15;
+            final int boxNumX=getWidth()/(boxWidth+boxGap);
+            final int boxNumY=5;
+            final int boxBaseX=((getWidth())-(boxWidth+boxGap)*boxNumX+boxGap)/2;
+            final int boxBaseY=50;
 
-                for(int y=0; y<boxNumY; y++) {
-                    for(int x=0; x<boxNumX; x++) {
-                        b.add(new Block(boxBaseX+(boxWidth+boxGap)*x, boxBaseY+(boxHeight+boxGap)*y, boxWidth, boxHeight));
-                        b.get(b.size()-1).setStrength(level);
-                    }
+            // Loop in a 2d array
+            for(int y=0; y<boxNumY; y++) {
+                for(int x=0; x<boxNumX; x++) {
+                    // Add a block at the current calculated x and y positions
+                    b.add(new Block(boxBaseX+(boxWidth+boxGap)*x, boxBaseY+(boxHeight+boxGap)*y, boxWidth, boxHeight));
+                    // Set the strength to the current level
+                    b.get(b.size()-1).setStrength(level);
                 }
-            //}
+            }
 
+            // Set the blocks array to the new block array
             blocks=b;
         }
 
+        // Utility method for getting a random ball
         private Ball randomBall() {
             Ball b=new Ball((int)(Math.random()*getWidth()), (int)(Math.round(Math.random()*getHeight()/2)));
             b.setDirection(randomDirectionPoint());
